e985018952c49829ce42e13fdb8fdc6ca713ac50
Matt Kramer <mkramer@lbl.gov>
2020-09-28 09:03:02 -0700

    Try adding some corrections to veto times

--- MuonVetoEff/analysis/muVetoEff.py
+++ MuonVetoEff/analysis/muVetoEff.py
@@ -8,8 +8,8 @@ def vetoEff(tfile, det, showerCut):
     n_ad = h_ad.Integral(h_ad.FindBin(3000), cutbin - 1)
     n_sh = h_ad.Integral(cutbin, h_ad.GetNbinsX())
 
-    t_wpVeto = n_wp * 600e-6
-    t_adVeto = n_ad * 1_400e-6
     t_shVeto = n_sh * 400_400e-6
+    t_adVeto = n_ad * 1_400e-6 * (1 - t_shVeto / livetime)
+    t_wpVeto = n_wp * 600e-6 * (1 - t_shVeto / livetime)
 
     return 1 - (t_wpVeto + t_adVeto + t_shVeto) / livetime
