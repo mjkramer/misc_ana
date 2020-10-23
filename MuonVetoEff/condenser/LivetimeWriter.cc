5b4faf5e1e9afb47628083def8d1c73cdf40c9f7
Matt Kramer <mkramer@lbl.gov>
2020-10-22 17:32:44 -0700

    Make LivetimeWriter save livetime as hist instead of param
    This way hadd does the right thing

--- MuonVetoEff/condenser/LivetimeWriter.cc
+++ MuonVetoEff/condenser/LivetimeWriter.cc
@@ -1,16 +1,10 @@
 #include "LivetimeWriter.hh"
 
 #include <TH1F.h>
-#include <TParameter.h>
 
 void LivetimeWriter::finalize(Pipeline& pipe)
 {
-  TFile* inFile = pipe.inFile();
-
-  auto h_livetime = (TH1F*) inFile->Get("h_livetime");
-  float livetime = h_livetime->GetBinContent(1);
-
-  auto p_livetime = new TParameter<float>("livetime", livetime);
+  auto h_livetime = (TH1F*) pipe.inFile()->Get("h_livetime");
   pipe.getOutFile()->cd();
-  p_livetime->Write();
+  h_livetime->Write();
 }
