7307e113191533436991d083388a85c59ca28113
Matt Kramer <mkramer@lbl.gov>
2020-12-01 21:55:07 -0800

    Add gen_cutlist.py

---
+++ MuonVetoEff/analysis/gen_cutlist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import argparse
+
+import numpy as np
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("min_pe", type=float)
+    ap.add_argument("max_pe", type=float)
+    ap.add_argument("n_pe", type=int)
+    ap.add_argument("min_sec", type=float)
+    ap.add_argument("max_sec", type=float)
+    ap.add_argument("n_sec", type=int)
+    args = ap.parse_args()
+
+    for pe in np.linspace(args.min_pe, args.max_pe, args.n_pe):
+        for sec in np.linspace(args.min_sec, args.max_sec, args.n_sec):
+            print(f"{pe} {sec}")
+
+
+if __name__ == '__main__':
+    main()
--- MuonVetoEff/analysis/gen_toy_job_input.py
+++ MuonVetoEff/analysis/gen_toy_job_input.py
@@ -2,23 +2,16 @@
 
 import argparse
 
-import numpy as np
-
 
 def main():
     ap = argparse.ArgumentParser()
+    ap.add_argument("cut_list_file")
     ap.add_argument("template_dir")
-    ap.add_argument("min_pe", type=float)
-    ap.add_argument("max_pe", type=float)
-    ap.add_argument("n_pe", type=int)
-    ap.add_argument("min_sec", type=float)
-    ap.add_argument("max_sec", type=float)
-    ap.add_argument("n_sec", type=int)
     args = ap.parse_args()
 
-    for pe in np.linspace(args.min_pe, args.max_pe, args.n_pe):
-        for sec in np.linspace(args.min_sec, args.max_sec, args.n_sec):
-            print(f"{args.template_dir} {pe} {sec}")
+    for line in open(args.cut_list_file):
+        pe, sec = line.strip().split()
+        print(f"{args.template_dir} {pe} {sec}")
 
 
 if __name__ == '__main__':
