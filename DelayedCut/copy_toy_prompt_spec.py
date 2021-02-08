85a6214c865aab7a089802475334e4ef68caec45
Matt Kramer <mkramer@lbl.gov>
2021-02-08 12:01:31 -0800

    Revert scaling of prompt spectra

--- DelayedCut/copy_toy_prompt_spec.py
+++ DelayedCut/copy_toy_prompt_spec.py
@@ -5,7 +5,6 @@ import argparse
 import ROOT as R
 
 from gen_text_for_toy import dets_for, idet
-from util import read_theta13_file
 
 
 def main():
@@ -21,13 +20,11 @@ def main():
         outfile = R.TFile(f"{args.outdir}/ibd_eprompt_shapes_{nADs}ad.root",
                           "RECREATE")
         t13file = f"{args.outdir}/Theta13-inputs_P17B_inclusive_{nADs}ad.txt"
-        del_effs = read_theta13_file(t13file)["delcut_eff"]
 
         for site in [1, 2, 3]:
             for det in dets_for(site):
                 i = idet(site, det)
                 h_in = infile.Get(f"h_nominal_stage{stage}_ad{i+1}")
-                h_in.Scale(del_effs[i])
                 outfile.cd()
                 h_in.Write(f"h_ibd_eprompt_inclusive_eh{site}_ad{det}")
 
