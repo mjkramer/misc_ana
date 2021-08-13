d5a432cc3367532728cf5d36d401c209d78cc9a1
Matt Kramer <mkramer@lbl.gov>
2021-08-12 21:13:09 -0700

    Update plot_energy_scale and plot_gain

--- MiscPlots/plot_gain.py
+++ MiscPlots/plot_gain.py
@@ -12,18 +12,23 @@ from util import SITEDETS, DAY0, DayLister
 
 
 DBNAME = "offline_db_ihep"
-DATAFILE = "data/energy_scale.csv"
+DATAFILE = "data/gain.csv"
 
 
-def get_energy_scale(db: DBConn, site, det, when):
+def get_gain(db: DBConn, site, det, when):
     sitemask = 4 if site == 3 else site
 
-    query = f"""SELECT peevis FROM EnergyRecon NATURAL JOIN EnergyReconVld
-    WHERE task=1 AND simmask=1 AND sitemask={sitemask} AND subsite={det}
+    query1 = f"""SELECT seqno FROM CalibPmtFineGainVld
+    WHERE simmask=1 AND sitemask={sitemask} AND subsite={det}
     AND timestart <= '{when}'
     ORDER BY versiondate DESC LIMIT 1"""
 
-    return db.execute(query).fetchone()[0]
+    seqno = db.execute(query1).fetchone()[0]
+
+    query2 = f"""SELECT avg(spehigh) FROM CalibPmtFineGain
+    WHERE seqno={seqno} AND row_counter < 192"""
+
+    return db.execute(query2).fetchone()[0]
 
 
 # Should be internal to 'dump', but we get an error
@@ -35,10 +40,10 @@ def get_rows(site, det):
     def gen():
         for day in days:
             absday = DAY0 + timedelta(days=day)
-            escale = get_energy_scale(db, site, det, absday)
-            print((day, site, det, escale), flush=True)
+            gain = get_gain(db, site, det, absday)
+            print((day, site, det, gain), flush=True)
             yield {"day": day, "site": site, "det": det,
-                   "escale": escale}
+                   "gain": gain}
 
     return list(gen())
 
@@ -51,7 +56,7 @@ def dump(fname=DATAFILE):
     data.to_csv(fname, index=False)
 
 
-def plot_energy_scale(fname=DATAFILE, **plotargs):
+def plot_gain(fname=DATAFILE, **plotargs):
     data = pd.read_csv(fname)
 
     fig, ax = plt.subplots()
@@ -61,15 +66,15 @@ def plot_energy_scale(fname=DATAFILE, **plotargs):
         subdata = data.query(f"site == {site} and det == {det}")
         days = [DAY0 + timedelta(days=day)
                 for day in subdata["day"]]
-        ax.plot(days, subdata["escale"], "o", label=label, ms=2, **plotargs)
+        ax.plot(days, subdata["gain"], "o", label=label, ms=2, **plotargs)
 
     ax.legend()
     ax.set_xlabel("Date")
-    ax.set_ylabel("PE/MeV")
-    ax.set_title("AdSimple energy scale")
+    ax.set_ylabel("ADC/PE")
+    ax.set_title("AD-averaged gain")
     fig.tight_layout()
 
-    fig.savefig("gfx/energy_scale.pdf")
+    fig.savefig("gfx/gain.pdf")
 
     return fig, ax
 
--- MiscPlots/util.py
+++ MiscPlots/util.py
@@ -1,5 +1,16 @@
 import ROOT as R
 
+from datetime import datetime
+import os
+
+import pandas as pd
+
+
+SITEDETS = [(1, 1), (1, 2), (2, 1), (2, 2),
+            (3, 1), (3, 2), (3, 3), (3, 4)]
+
+DAY0 = datetime(2011, 12, 24)
+
 
 def dets_for(hall, nADs):
     if hall == 1:
@@ -19,3 +30,25 @@ def keep(o):
     except Exception:
         pass                     # unless you weren't going to anyway
     return o
+
+
+class DayLister:
+    def __init__(self):
+        fithome = os.environ["LBNL_FIT_HOME"]
+        path = f"{fithome}/ReactorPowerCalculator/dbd_livetime_P17B.txt"
+        self.df = pd.read_csv(path, sep=r"\s+",
+                              names=["day", "site", "livetime"])
+
+    def days_for(self, site, det):
+        sitedays = self.df.query(f"site == {site}")["day"]
+        daymin, daymax = self._minmax_days(site, det)
+        return [day for day in sitedays
+                if daymin <= day <= daymax]
+
+    def _minmax_days(self, site, det):
+        if (site, det) in [(2, 2), (3, 4)]:
+            return (218, 2076)
+        elif (site, det) == (1, 1):
+            return (0, 1824)
+        else:
+            return (0, 2076)
