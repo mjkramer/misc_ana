92750bbff61c637fa86d37c6d19d6a3c385fcbee
Matt Kramer <matt314159@gmail.com>
2021-10-18 14:14:46 -0700

    Update sideBySideRates.jl

--- MiscPlots/SideBySideRates/sideBySideRates.jl
+++ MiscPlots/SideBySideRates/sideBySideRates.jl
@@ -66,3 +66,48 @@ function sidebyside_data(ndets::AbstractArray=[6, 8, 7])
                      meas_rates = meas_rates |> nan2missing,
                      meas_errs = meas_errs |> nan2missing)
 end
+
+function plot_lianghong()
+    labels = ["AD1/AD2", "AD3/AD8", "AD4/EH3", "AD5/EH3", "AD6/EH3",
+              "AD4/EH3", "AD5/EH3", "AD6/EH3", "AD7/EH3"]
+
+    df68 = sidebyside_data([6, 8])
+    df78 = sidebyside_data([7, 8])
+    df6 = sidebyside_data([6])
+
+    function getvals(df, plotdets, normdets)
+        pred_norm = mean(df.pred_rates[normdets])
+        meas_norm = mean(df.meas_rates[normdets])
+
+        pred_ratios = df.pred_rates[plotdets] ./ pred_norm
+        meas_ratios = df.meas_rates[plotdets] ./ meas_norm
+        meas_ratio_errs = df.meas_errs[plotdets] ./ meas_norm
+        return pred_ratios, meas_ratios, meas_ratio_errs
+    end
+
+    function build(desc)
+        unwrap(l) = cat(l..., dims=1) # flatten array of arrays
+        # return @pipe (getvals.(myzip(desc))...) |> myzip |> map(unwrap, _)
+
+        results = (getvals(args...) for args in desc)
+        pred_wrap, meas_wrap, meas_err_wrap = myzip(results)
+        return map(unwrap, (pred_wrap, meas_wrap, meas_err_wrap))
+    end
+
+    ypred, ymeas, ymeas_err =
+        build([(df68, [1], [2]),
+               (df78, [3], [4]),
+               (df6, [5, 6, 7], [5, 6, 7]),
+               (df78, [5, 6, 7, 8], [5, 6, 7, 8])
+               ])
+
+    xpred_hack = [0.5, eachindex(labels)..., length(labels)+0.5]
+    ypred_hack = [ypred[1], ypred..., ypred[end]]
+    plot(xpred_hack, ypred_hack, st=:stepmid, ls=:dash, c=:red, label="Observed")
+
+    scatter!(ymeas, yerror=ymeas_err, xerror=0.5, c=:black, label="Expected")
+    vline!([1.5, 2.5, 5.5], ls=:dash, c=:black, label=nothing)
+    xticks!(eachindex(labels), labels)
+    ylabel!("Ratio of IBD rates")
+    title!("P17B IBD rate comparison")
+end
