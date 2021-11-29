e986fc620fdba5b9abbd89d3491353caf9f8d5dc
Matt Kramer <mkramer@lbl.gov>
2021-11-28 21:23:30 -0800

    Update plot_peakmaps.py

--- MiscPlots/NewNonUni/plot_peakmaps.py
+++ MiscPlots/NewNonUni/plot_peakmaps.py
@@ -7,8 +7,8 @@ import pandas as pd
 
 from dump_peaks import SELS, DIVS_R2, DIVS_Z
 
-CORRNAMES = {'post17_v5v3v1_NL@test_newNonUni_alphas_ngd': "alphas + nGd",
-             'post17_v5v3v1_NL@test_newNonUni_alphas_only': "alphas only",
+CORRNAMES = {'post17_v5v3v1_NL@test_newNonUni_alphas_ngd': "$\\alpha$+nGd",
+             'post17_v5v3v1_NL@test_newNonUni_alphas_only': "$\\alpha$-only",
              'post17_v5v3v1_NL@test_newNonUni_off': "no corr."}
 
 plt.rcParams.update({'font.size': 13})
@@ -22,16 +22,20 @@ def midZ(binZ):
     return 1e-3 * 0.5 * (DIVS_Z[binZ] + DIVS_Z[binZ - 1])
 
 
-def get_df(sel, site, det):
-    full_df = pd.read_csv(f'data/peakmaps/{sel}/peaks_nGd.csv')
+def get_df(sel, site, det, peak, *, relGdLS=False):
+    full_df = pd.read_csv(f'data/peakmaps/{sel}/peaks_{peak}.csv')
     df = full_df.query(f'site == {site} and det == {det}').copy()
     df["midR2"] = df["binR2"].map(midR2)
     df["midZ"] = df["binZ"].map(midZ)
+    if relGdLS:
+        sub_df = df.query('binR2 <= 6 and binZ >= 2 and binZ <= 9')
+        avg = sub_df['fit_peak'].mean()
+        df['fit_peak'] /= avg
     return df
 
 
-def plot_nGd(df, title, **kwargs):
-    piv = df.pivot_table(columns="midR2", index="midZ", values="peak_nGd")
+def plot_df(df, title, **kwargs):
+    piv = df.pivot_table(columns="midR2", index="midZ", values="fit_peak")
     plt.pcolormesh(piv.columns, piv.index, piv.values,
                    shading="nearest", **kwargs)
     plt.colorbar()
@@ -42,13 +46,13 @@ def plot_nGd(df, title, **kwargs):
 
 
 def get_extrema(dfs):
-    vmin = min(df['peak_nGd'].min() for df in dfs)
-    vmax = max(df['peak_nGd'].max() for df in dfs)
+    vmin = min(df['fit_peak'].min() for df in dfs)
+    vmax = max(df['fit_peak'].max() for df in dfs)
     return vmin, vmax
 
 
-def plot_nGd_grid(site, det):
-    dfs = [get_df(sel, site, det) for sel in SELS]
+def plot_grid(site, det, peak, relGdLS=False):
+    dfs = [get_df(sel, site, det, peak, relGdLS=relGdLS) for sel in SELS]
     vmin, vmax = get_extrema(dfs)
 
     fig, axs = plt.subplots(2, 2, figsize=(14.8, 9.6))
@@ -56,21 +60,23 @@ def plot_nGd_grid(site, det):
 
     for sel, df, ax in zip(SELS, dfs, axs.flatten()):
         plt.sca(ax)
-        title = f'nGd peak ({CORRNAMES[sel]}), EH{site}-AD{det}, post-P17B'
-        plot_nGd(df, title, vmin=vmin, vmax=vmax)
+        suffix = ' (rel. GdLS)' if relGdLS else ''
+        title = f'{peak} peak ({CORRNAMES[sel]}), EH{site}-AD{det}{suffix}'
+        plot_df(df, title, vmin=vmin, vmax=vmax)
 
     fig.tight_layout()
 
     tag = SELS[0].split("@")[0]  # e.g. post17_v5v3v1_NL
-    gfxdir = f'gfx/nGd_grid/{tag}'
+    name = 'gridRel' if relGdLS else 'grid'
+    gfxdir = f'gfx/{name}/{tag}'
     os.system(f'mkdir -p {gfxdir}')
     for ext in ['pdf', 'png']:
-        fig.savefig(f'{gfxdir}/nGd_grid_eh{site}_ad{det}.{ext}')
+        fig.savefig(f'{gfxdir}/{name}_{peak}_eh{site}_ad{det}.{ext}')
 
 
-def plot_nGd_grid_all():
+def plot_grid_all(peak, relGdLS=False):
     sites = [1, 2, 2, 3, 3, 3, 3]
     dets = [2, 1, 2, 1, 2, 3, 4]
 
     for site, det in zip(sites, dets):
-        plot_nGd_grid(site, det)
+        plot_grid(site, det, peak, relGdLS)
