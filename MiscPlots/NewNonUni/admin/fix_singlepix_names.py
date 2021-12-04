eec258f558608bd90d626957bf43d58999b125ac
Matt Kramer <matt314159@gmail.com>
2021-12-04 09:47:07 -0800

    Add admin/fix_singlepix_names.py

---
+++ MiscPlots/NewNonUni/admin/fix_singlepix_names.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from os.path import isfile
+from shutil import copy
+
+from common import SELS
+import MyROOT as R
+
+
+def fix_name(name: str) -> str:
+    parts = name.split('_')
+    for i in [4, 6]:
+        parts[i] = str(int(parts[i]) + 1)
+    return '_'.join(parts)
+
+
+def fix_names(sel: str, site: int):
+    direc = f'input/{sel}'
+    fname = f'stage2.pbp.eh{site}.7ad.root'
+    path = f'{direc}/{fname}'
+    origpath = f'{direc}/orig.{fname}'
+
+    if not isfile(origpath):
+        copy(path, origpath)
+
+    src = R.TFile(origpath)
+    dest = R.TFile(path, "RECREATE")
+
+    for key in src.GetListOfKeys():
+        name = key.GetName()
+        if name.startswith('h_single') and name.find('r2') != -1:
+            newname = fix_name(name)
+        else:
+            newname = name
+        obj = src.Get(name)
+        dest.cd()
+        obj.Write(newname)
+
+    dest.Close()
+    src.Close()
+
+
+def main():
+    for sel in SELS:
+        for site in [1, 2, 3]:
+            fix_names(sel, site)
+
+
+if __name__ == '__main__':
+    main()
