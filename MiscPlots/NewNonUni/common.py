f884fc69f5080a1d24f2cce5f902469b4a3e766a
Matt Kramer <matt314159@gmail.com>
2021-12-05 08:55:43 -0800

    Update plot_peakmaps (flexible wrt tags)

--- MiscPlots/NewNonUni/common.py
+++ MiscPlots/NewNonUni/common.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python-3
 
+from collections import OrderedDict
+
 import numpy as np
 
 DIVS_R2 = np.linspace(0., 4e6, 11)
 DIVS_Z = np.linspace(-2e3, 2e3, 11)
 
-SELS = ['post17_v5v3v1_NL@test_newNonUni_alphas_ngd',
-        'post17_v5v3v1_NL@test_newNonUni_alphas_only',
-        'post17_v5v3v1_NL@test_newNonUni_off']
+_CONFIGS = [('test_newNonUni_alphas_ngd', "$\\alpha$+nGd"),
+            ('test_newNonUni_alphas_only',  "$\\alpha$-only"),
+            ('test_newNonUni_off',  "no corr.")]
+CONFIGS = OrderedDict(_CONFIGS)
+
+_TAGS = [('p17b_v4_NL', 'P17B'), ('post17_v5v3v1_NL', 'Post17')]
+TAGS = OrderedDict(_TAGS)
--- MiscPlots/NewNonUni/plot_peakmaps.py
+++ MiscPlots/NewNonUni/plot_peakmaps.py
@@ -6,11 +6,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from common import SELS, DIVS_R2, DIVS_Z
-
-CORRNAMES = {'post17_v5v3v1_NL@test_newNonUni_alphas_ngd': "$\\alpha$+nGd",
-             'post17_v5v3v1_NL@test_newNonUni_alphas_only': "$\\alpha$-only",
-             'post17_v5v3v1_NL@test_newNonUni_off': "no corr."}
+from common import CONFIGS, DIVS_R2, DIVS_Z, TAGS
 
 plt.rcParams.update({'font.size': 13})
 
@@ -23,8 +19,9 @@ def midZ(binZ):
     return 1e-3 * 0.5 * (DIVS_Z[binZ] + DIVS_Z[binZ - 1])
 
 
-def get_df(sel, site, det, peak, *, relGdLS=False):
-    full_df = pd.read_csv(f'data/peakmaps/{sel}/peaks_{peak}.csv')
+def get_df(tag: str, config: str, site: int, det: int, peak: str,
+           *, relGdLS=False):
+    full_df = pd.read_csv(f'data/peakmaps/{tag}@{config}/peaks_{peak}.csv')
     assert isinstance(full_df, pd.DataFrame)
     df = full_df.query(f'site == {site} and det == {det}').copy() # type:ignore
     assert isinstance(df, pd.DataFrame)
@@ -56,8 +53,18 @@ def get_extrema(dfs):
     return vmin, vmax
 
 
-def plot_grid(site, det, peak, *, relGdLS=False, autorange=False, **kwargs):
-    dfs = [get_df(sel, site, det, peak, relGdLS=relGdLS) for sel in SELS]
+def get_global_extrema(tag: str, peak: str, *, relGdLS: bool):
+    sites = [1, 2, 2, 3, 3, 3, 3]
+    dets = [2, 1, 2, 1, 2, 3, 4]
+    dfs = [get_df(tag, config, site, det, peak, relGdLS=relGdLS)
+           for config in CONFIGS.keys()
+           for (site, det) in zip(sites, dets)]
+    return get_extrema(dfs)
+
+
+def plot_grid(tag, site, det, peak, *, relGdLS=False, autorange=False, **kwargs):
+    dfs = [get_df(tag, config, site, det, peak, relGdLS=relGdLS)
+           for config in CONFIGS.keys()]
     if autorange:
         vmin, vmax = get_extrema(dfs)
         kwargs |= dict(vmin=vmin, vmax=vmax)
@@ -67,38 +74,47 @@ def plot_grid(site, det, peak, *, relGdLS=False, autorange=False, **kwargs):
     axs = [fig.add_subplot(gs[r:r+2, c:c+2])
            for (r, c) in [(0, 0), (0, 2), (2, 1)]]
 
-    for sel, df, ax in zip(SELS, dfs, axs):
+    for config, df, ax in zip(CONFIGS.keys(), dfs, axs):
         plt.sca(ax)
-        suffix = ' (rel. GdLS)' if relGdLS else ''
-        title = f'{peak} peak ({CORRNAMES[sel]}), EH{site}-AD{det}{suffix}'
+        # suffix = ' (rel. GdLS)' if relGdLS else ''
+        title = f'{peak} peak ({CONFIGS[config]}), EH{site}-AD{det}, {TAGS[tag]}'
         plot_df(df, title, **kwargs)
 
     fig.tight_layout()
 
-    tag = SELS[0].split("@")[0]  # e.g. post17_v5v3v1_NL
     name = 'gridRel' if relGdLS else 'grid'
-    gfxdir = f'gfx/{name}/{tag}'
+    gfxdir = f'gfx/{name}/{TAGS[tag]}/{peak}'
     os.system(f'mkdir -p {gfxdir}')
-    for ext in ['pdf', 'png', 'svg', 'eps']:
+    # for ext in ['pdf', 'png', 'svg', 'eps']:
+    for ext in ['png']:
         fig.savefig(f'{gfxdir}/{name}_{peak}_eh{site}_ad{det}.{ext}')
+    return gfxdir
 
 
-def plot_grid_all(peak, relGdLS=False):
+def plot_grid_all(tag, peak, /, **kwargs):
     sites = [1, 2, 2, 3, 3, 3, 3]
     dets = [2, 1, 2, 1, 2, 3, 4]
 
+    vmin, vmax = get_global_extrema(tag, peak, relGdLS=True)
+
+    gfxdir = ''                 # silence warning about being possibly unbound
     for site, det in zip(sites, dets):
-        plot_grid(site, det, peak, relGdLS=relGdLS)
+        gfxdir = plot_grid(tag, site, det, peak,
+                           relGdLS=True, colorbar=False, autorange=False,
+                           vmin=vmin, vmax=vmax, **kwargs)
+
+    cbar = just_colorbar(vmin, vmax)
+    cbar.savefig(f'{gfxdir}/colorbar.png')
 
 
-def plot_grid_all_all(relGdLS=False):
+def plot_grid_all_all(tag, /, **kwargs):
     for peak in ['nGd', 'nGdExp', 'nGdDyb1', 'nGdDyb2']:
-        plot_grid_all(peak, relGdLS)
+        plot_grid_all(tag, peak, **kwargs)
 
 
-def plot_grid_all_all_singles(relGdLS=False):
+def plot_grid_all_all_singles(tag, /, **kwargs):
     for peak in ['K40', 'Tl208']:
-        plot_grid_all(peak, relGdLS)
+        plot_grid_all(tag, peak, **kwargs)
 
 
 def just_colorbar(vmin: float, vmax: float):
@@ -107,3 +123,4 @@ def just_colorbar(vmin: float, vmax: float):
     cmap = plt.rcParams['image.cmap']
     norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax) # type:ignore
     fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), cax=ax) # type:ignore
+    return fig
